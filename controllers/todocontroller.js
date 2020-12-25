039074178f7f575f35b22de88bb9c69b2f540765
ahmed emad <ahmed.0881100@gmail.com>
2020-12-25 23:28:54 +0200

    starting commit

---
+++ controllers/todocontroller.js
@@ -0,0 +1,19 @@
+module.exports= (app)=>{
+    app.get('/todo',(req,res)=>{
+         
+    })
+    app.post('url',(req,res)=>{
+
+    })
+    app.delete('url',(req,res)=>{
+        
+    })
+
+
+
+
+
+
+
+
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const todoController = require('./controllers/todocontroller');
+
+const app = express();
+
+//template engine setup
+app.set('view engine','ejs');
+
+// servew static file 
+app.use(express.static('./assets'))
+//controllers
+todoController(app);
+
+// listen to port 
+app.listen(3000);
+console.log('port 3000');
